7d0c11fdd1c0cc34ba074b35078a8f19637392b9
SERGIO VELASQUEZ <sergio_velasquez@bhdleon.com.do>
2022-08-02 17:31:15 -0500

    Lesson 59 - list 2 (ion-item-sliding)

--- src/app/pages/list/list.page.ts
+++ src/app/pages/list/list.page.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonList } from '@ionic/angular';
 import { Observable } from 'rxjs';
 import { DataService } from 'src/app/services/data.service';
 
@@ -9,14 +10,29 @@ import { DataService } from 'src/app/services/data.service';
 })
 export class ListPage implements OnInit {
 
+  @ViewChild(IonList) ionList: IonList;
+
   usuarios: Observable<any>;
 
   constructor(private dataService: DataService) { }
 
-   ngOnInit() {
-    this.usuarios = this.dataService.getUsuarios();
+  ngOnInit() {
   }
 
+  favorite(user: any) {
+    console.log('Favorite: ' + user);
+    this.ionList.closeSlidingItems();
+  }
+
+  share(user: any) {
+    console.log('Share: ' + user);
+    this.ionList.closeSlidingItems();
+  }
+
+  delete(user: any) {
+    console.log('Delete: ' + user.name);
+    this.ionList.closeSlidingItems();
+  }
 
 
 }

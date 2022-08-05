32d93955859037b709b64c7e2de3680e5a081203
SERGIO VELASQUEZ <sergio_velasquez@bhdleon.com.do>
2022-08-05 15:11:16 -0500

    Lesson 80. toast

---
+++ src/app/pages/toast/toast.module.ts
@@ -0,0 +1,22 @@
+import { ComponentsModule } from 'src/app/components/components.module';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+import { IonicModule } from '@ionic/angular';
+
+import { ToastPageRoutingModule } from './toast-routing.module';
+
+import { ToastPage } from './toast.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ToastPageRoutingModule,
+    ComponentsModule
+  ],
+  declarations: [ToastPage]
+})
+export class ToastPageModule {}
